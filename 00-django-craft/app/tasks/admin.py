8d0bddc333257d6767d1f340bab0c5f5d73bd300
Jose Miguel <josemiguel.lopezperez.ext@telefonica.com>
2024-02-23 19:54:03 +0100

    Tasks

--- 00-django-craft/app/tasks/admin.py
+++ 00-django-craft/app/tasks/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 
 from .models import Task,Tag,Worker, Customer, Project, Epic, Skill
 
-admin.site.register(Tag)
-admin.site.register(Skill)
-admin.site.register(Worker)
-admin.site.register(Customer)
-admin.site.register(Project)
-admin.site.register(Epic)
-admin.site.register(Task)
+# admin.site.register(Tag)
+# admin.site.register(Skill)
+# admin.site.register(Worker)
+# admin.site.register(Customer)
+# admin.site.register(Project)
+# admin.site.register(Epic)
+# admin.site.register(Task)
